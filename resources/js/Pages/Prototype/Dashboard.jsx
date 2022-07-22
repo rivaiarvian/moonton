d7f4bb44b9a53b06f7b2a1791b86d44a85e1fd58
rivaiarvian <rivai.arviannn@gmail.com>
2022-07-22 21:36:28 +0700

    feat: slicing dashboard - Browse movie card

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,3 +1,4 @@
+import MovieCard from "@/Components/MovieCard";
 import FeatureMovie from "@/Components/FeatureMovie";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import { Head } from "@inertiajs/inertia-react";
@@ -20,13 +21,13 @@ function Dashboard() {
                     rel="stylesheet"
                     href="https://unpkg.com/flickity@2/dist/flickity.min.css"
                 />
+                <title>Dashboard</title>
             </Head>
             <div>
                 <div className="font-semibold text-[22px] text-black mb-4">
                     Featured Movies
                 </div>
                 <Flickity className="gap-[30px]" options={flickityOptions}>
-                    {/* Movie thumbnail */}
                     {[1, 2, 3, 4].map((i) => (
                         <FeatureMovie
                             key={i}
@@ -35,6 +36,23 @@ function Dashboard() {
                             category="Action • Horror"
                             thumbnail="/assets/images/featured-1.png"
                             rating="4.5/5.0"
+                        />
+                    ))}
+                </Flickity>
+            </div>
+
+            <div className="mt-[50px]">
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Browse
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+                        <MovieCard
+                            key={i}
+                            slug=""
+                            name="Meong Golden"
+                            category="Horror • Love"
+                            thumbnail="/assets/images/browse-1.png"
                         />
                     ))}
                 </Flickity>
