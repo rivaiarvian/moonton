6262fcb30c55e2db8da44e041e7c92c567b287d7
rivaiarvian <rivai.arviannn@gmail.com>
2022-07-22 21:04:53 +0700

    feat: slicing dashboard - feature movie

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,9 +1,46 @@
-import Authenticated from '@/Layouts/Authenticated/Index'
+import FeatureMovie from "@/Components/FeatureMovie";
+import Authenticated from "@/Layouts/Authenticated/Index";
+import { Head } from "@inertiajs/inertia-react";
+import Flickity from "react-flickity-component";
 
 function Dashboard() {
-  return (
-    <Authenticated/>
-  )
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
+    return (
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {/* Movie thumbnail */}
+                    {[1, 2, 3, 4].map((i) => (
+                        <FeatureMovie
+                            key={i}
+                            slug=""
+                            name="The Batman in Love"
+                            category="Action • Horror"
+                            thumbnail="/assets/images/featured-1.png"
+                            rating="4.5/5.0"
+                        />
+                    ))}
+                </Flickity>
+            </div>
+        </Authenticated>
+    );
 }
 
-export default Dashboard
+export default Dashboard;
